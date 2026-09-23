ace5ed0cfd4a33ce2f02193a32e329b397412b7b
guptadeepak04 <deepakgupta6935@gmail.com>
2026-09-23 10:36:24 +0000

    terraform update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Project     = "Jerney"
+      Project     = "Three-tier-project"
       Environment = var.environment
       ManagedBy   = "Terraform"
     }
